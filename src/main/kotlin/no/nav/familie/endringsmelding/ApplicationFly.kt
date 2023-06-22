8495869555b1416675cc1ddc8c15e23dc7cc3c99
halvorbmundal <halvorbmundal@users.noreply.github.com>
2023-06-22 15:18:15 +0200

    Slå sammen bygg og deploy (#5)
    * Må opprette ApplicationFly som kan brukes i fly.io for å ikke krasje testene
    
    * Fjern kommentert ut kode
    
    * Slå sammen bygg og deploy
    
    * Midlertidig rename
    
    * Rename
    
    * Fjern krav om at branch må være main
    
    * Ikke bruk alpine
    
    * Bruk alpine igjen
    
    * Fjern dobbel checkout
    
    * Forsøker å sette domene til frontend for cookien som skal brukes
    
    ---------
    
    Co-authored-by: charliemidtlyng <charlie.midtlyng@bekk.no>

--- src/main/kotlin/no/nav/familie/endringsmelding/ApplicationFly.kt
+++ src/main/kotlin/no/nav/familie/endringsmelding/ApplicationFly.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.endringsmelding
 
+import no.nav.familie.endringsmelding.mock.EnableMockOAuth2ServerFly
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
@@ -7,9 +8,10 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 
 @ConfigurationPropertiesScan
 @SpringBootApplication(scanBasePackages = ["no.nav.familie.endringsmelding"])
+@EnableMockOAuth2ServerFly // TODO: SLett når vi ikke bruker fly.io
 @EnableJwtTokenValidation(ignore = ["org.springframework", "org.springdoc"])
-class Application
+class ApplicationFly
 
 fun main(args: Array<String>) {
-    SpringApplicationBuilder(Application::class.java).run(*args)
+    SpringApplicationBuilder(ApplicationFly::class.java).run(*args)
 }
