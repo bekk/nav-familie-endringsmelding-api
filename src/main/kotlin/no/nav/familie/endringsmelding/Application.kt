63a0c6f16c6f13e6a87512f933a884e149ef1510
charliemidtlyng <charlie.midtlyng@bekk.no>
2023-06-22 10:34:13 +0200

    Skal støtte mock-login på fly.io

--- src/main/kotlin/no/nav/familie/endringsmelding/Application.kt
+++ src/main/kotlin/no/nav/familie/endringsmelding/Application.kt
@@ -1,11 +1,13 @@
 package no.nav.familie.endringsmelding
 
+import no.nav.familie.endringsmelding.mock.EnableMockOAuth2Server
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 
 @ConfigurationPropertiesScan
+@EnableMockOAuth2Server // TODO: SLett når vi ikke bruker fly.io
 @SpringBootApplication(scanBasePackages = ["no.nav.familie.endringsmelding"])
 @EnableJwtTokenValidation(ignore = ["org.springframework", "org.springdoc"])
 class Application
---
+++ src/main/kotlin/no/nav/familie/endringsmelding/mock/PdlClientConfig.kt
@@ -0,0 +1,100 @@
+package no.nav.familie.endringsmelding.mock
+
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.familie.endringsmelding.integration.PdlClient
+import no.nav.familie.endringsmelding.integration.dto.pdl.Bostedsadresse
+import no.nav.familie.endringsmelding.integration.dto.pdl.Familierelasjonsrolle
+import no.nav.familie.endringsmelding.integration.dto.pdl.ForelderBarnRelasjon
+import no.nav.familie.endringsmelding.integration.dto.pdl.Navn
+import no.nav.familie.endringsmelding.integration.dto.pdl.PdlSøker
+import no.nav.familie.endringsmelding.integration.dto.pdl.Sivilstand
+import no.nav.familie.endringsmelding.integration.dto.pdl.Sivilstandstype
+import no.nav.familie.endringsmelding.integration.dto.pdl.Statsborgerskap
+import no.nav.familie.endringsmelding.integration.dto.pdl.Vegadresse
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.Profile
+import java.time.LocalDate
+
+// TODO: Slett når vi ikke bruker fly.io
+@Configuration
+@Profile("fly")
+class PdlClientConfig {
+
+    private val startdato = LocalDate.of(2020, 1, 1)
+    private val sluttdato = LocalDate.of(2021, 1, 1)
+
+    @Bean
+    @Primary
+    fun pdlClient(): PdlClient {
+        val pdlClient: PdlClient = mockk()
+
+        every { pdlClient.ping() } returns Unit
+
+        every { pdlClient.hentSøker(any()) } returns
+            PdlSøker(
+                adressebeskyttelse = listOf(),
+                bostedsadresse = bostedsadresse(),
+                forelderBarnRelasjon = lagBarn(),
+                navn = lagNavn(),
+                sivilstand = sivilstand(),
+                statsborgerskap = statsborgerskap(),
+            )
+
+        return pdlClient
+    }
+
+    private fun lagBarn(): List<ForelderBarnRelasjon> {
+        return listOf(ForelderBarnRelasjon("28021078036", Familierelasjonsrolle.BARN))
+    }
+
+    private fun lagNavn(
+        fornavn: String = "Fornavn",
+        mellomnavn: String? = "mellomnavn",
+        etternavn: String = "Etternavn",
+    ): List<Navn> =
+        listOf(
+            Navn(
+                fornavn,
+                mellomnavn,
+                etternavn,
+            ),
+        )
+
+    private fun statsborgerskap(): List<Statsborgerskap> =
+        listOf(
+            Statsborgerskap(
+                land = "NOR",
+                gyldigFraOgMed = startdato,
+                gyldigTilOgMed = null,
+            ),
+            Statsborgerskap(
+                land = "SWE",
+                gyldigFraOgMed = startdato.minusYears(3),
+                gyldigTilOgMed = startdato,
+            ),
+        )
+
+    private fun sivilstand(): List<Sivilstand> =
+        listOf(Sivilstand(type = Sivilstandstype.SKILT))
+
+    private fun bostedsadresse(): List<Bostedsadresse> =
+        listOf(
+            Bostedsadresse(
+                vegadresse = vegadresse(),
+                matrikkeladresse = null,
+            ),
+        )
+
+    private fun vegadresse(): Vegadresse =
+        Vegadresse(
+            husnummer = "13",
+            husbokstav = "b",
+            adressenavn = "Charlies vei",
+            postnummer = "0575",
+            bruksenhetsnummer = "",
+            matrikkelId = 1L,
+        )
+}
